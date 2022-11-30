ec5ac1def502bfd1f09e5b9d81bd71f91ef10cb4
lilly <lilly1987@naver.com>
2022-11-30 11:50:42 +0900

    Update 용량 분류.ps1

--- 용량 분류.ps1
+++ 용량 분류.ps1
@@ -1,22 +1,23 @@
-[int]$cnt=0
+[int]$max=(1024*1024*49)
+[int]$cnt=$max
 [int]$num=1
-While(Test-Path $num){
-    $num++
-}
-New-Item -Path "$num" -ItemType Directory -Force
 Get-ChildItem -Path "bak\*png" |
 	ForEach-Object { 'BEGIN' } { 
 		#$_.Length
 		#$cnt
 		$cnt+=$_.Length
-		if( $cnt -gt (1024*1024*49) ){
+		if( $cnt -gt $max ){
 			While(Test-Path $num){
 				$num++
 			}
 			New-Item -Path "$num" -ItemType Directory -Force
 			$cnt=0
 		}
+		#$_ | Select-Object -Property *
+		([WildcardPattern]::Escape($_.DirectoryName + '\' + $_.BaseName + '.txt' ))
+		Move-Item -Destination "$num" -Path ([WildcardPattern]::Escape($_.DirectoryName + '\' + $_.BaseName + '.txt' ))
 		Move-Item -Destination "$num" -Path ([WildcardPattern]::Escape($_))
 	} { 'END' }
 $cnt
 Read-Host "END"
+
