ca717675d79e94c711b00aa04678081f9059b447
lilly <lilly1987@naver.com>
2022-12-02 00:31:02 +0900

    Create add num.ps1

---
+++ add num.ps1
@@ -0,0 +1,29 @@
+# Set-ExecutionPolicy RemoteSigned
+
+$filename = Read-Host -Prompt 'Input your file  name'
+#$filename = "mynset.txt"
+
+if( -not( Test-Path -LiteralPath "$filename" ) ){
+	return
+}
+
+$myarray = [System.Collections.ArrayList]::new()
+
+$FILE = Get-Content "$filename"
+$FILE | ForEach-Object {
+	$_ | Select-Object -Property *
+	if ( $_ -match ",[\-_ ]*\d+_*[, ]?$" ){
+		$Matches		
+		[void]$myArray.Add($_)
+	}else{
+		$Matches
+		$hash=$_.GetHashCode();
+		[void]$myArray.Add("$_, $hash")
+	}
+}
+
+[Environment]::CurrentDirectory = (Get-Location -PSProvider FileSystem).ProviderPath
+[System.IO.File]::WriteAllLines("./${filename}", $myArray)
+#$myArray | Out-File "${filename}.rst"
+
+Read-Host "END"
