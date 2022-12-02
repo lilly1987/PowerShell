536b65c15b5ed3064772d905e327783ee09a1505
lilly <lilly1987@naver.com>
2022-12-02 19:58:40 +0900

    Update line add hash.ps1

--- line add hash.ps1
+++ line add hash.ps1
@@ -9,9 +9,9 @@ if( -not( Test-Path -LiteralPath "$filename" ) ){
 
 $myarray = [System.Collections.ArrayList]::new()
 
-$FILE = Get-Content "$filename"
+$FILE = Get-Content "$filename" -Encoding UTF8
 $FILE | ForEach-Object {
-	$_ | Select-Object -Property *
+	#$_ | Select-Object -Property *
 	if ( $_ -match ",[\-_ ]*\d+_*[, ]?$" ){
 		$Matches		
 		[void]$myArray.Add($_)
@@ -20,10 +20,11 @@ $FILE | ForEach-Object {
 		$hash=$_.GetHashCode();
 		[void]$myArray.Add("$_, $hash")
 	}
+	$_
 }
 
 [Environment]::CurrentDirectory = (Get-Location -PSProvider FileSystem).ProviderPath
-[System.IO.File]::WriteAllLines("./${filename}", $myArray)
+[System.IO.File]::WriteAllLines("${filename}.rst", $myArray)
 #$myArray | Out-File "${filename}.rst"
 
 Read-Host "END"
